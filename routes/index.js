6009633bf7fca4b0d3ef54aa344646a74c696455
gnedivad <gnedivad@gmail.com>
2015-11-21 15:38:05 -0800

    Added word search solver - Added getStringMeta, findAllWords, inBounds, findAllWordsHelper to   public/js/index.js - Added (*) to api route in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var dictionary = require('../lib/dictionary');
 
 module.exports = function(app) {
-  app.get('/trie/:string', function(request, response) {
-    var string = request.params.string;
+  app.get('/', function(request, response) {
+    response.render('index.html');
+  })
+
+  app.get('/api/:string(*)', function(request, response) {
+    var string = request.params.string.toLowerCase();
     response.status(200).json({
       string: string,
       isWord: dictionary.contains(string),
