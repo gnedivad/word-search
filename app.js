5e09b434636e31ab73cb5c6ada42f354a9bcb00c
gnedivad <gnedivad@gmail.com>
2015-11-24 04:03:52 -0800

    Changed port from 8888 to 1337

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var app = express();
 require('./routes/index.js')(app);
 require('./settings')(app, express);
 
-app.listen(process.env.PORT || 8888);
-console.log("Listening at 127.0.0.1:" + 8888);
+app.listen(process.env.PORT || 1337);
+console.log("Listening at 127.0.0.1:" + 1337);
